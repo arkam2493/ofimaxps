c616000c8a125d74f9aaa158335aaacec1492665
arkam <40009713+arkam2493@users.noreply.github.com>
2021-03-26 22:16:59 -0300

    Update ruta principal

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::group(['middleware' => 'auth'], function()
 {
